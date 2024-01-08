5a7b40af01f2286d1fa6a83662c043da06c813bc
Jishnuck2000 <jeochirrakkal26@gmail.com>
2024-01-08 23:07:18 +0530

    Initial commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Home.css'
+import { ParallaxBanner, useParallax } from "react-scroll-parallax";
+
+
+function Home() {
+
+  const parallax = useParallax({
+    scale: [0.1,2, "easeInQuad"],
+    speed:-50,
+  });
+  const dubai = useParallax({
+    scale: [1,0, "easeInQuad"],
+    speed:-50,
+  });
+
+  const parallaxEasing = useParallax({
+    easing: "easeOutQuad",
+    translateX: [-340, 100],
+  });
+  const parallaxEasingLeft = useParallax({
+    easing: [1, -0.75, 0.5, 1.34],
+    translateX: [0, -260],
+    translateY: [1100],
+  })
+
+  return (
+    <div className="container-body">
+      <div>
+      <img
+     ref={parallax.ref}
+     src='balloon.png'
+      className="balloon"></img>
+      </div>
+
+<div className="absolute-text">
+          <h1 ref={parallaxEasing.ref}>WELCOME TO DUBAI</h1>
+          <h2 ref={parallaxEasingLeft.ref}>Vist For All Bookings</h2>
+        </div>
+<div className='d'>
+        <img
+     ref={dubai.ref}
+     src='dubai.jpg'
+      className="dubai"></img>
+</div>
+
+       
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+   )
+}
+
+export default Home
